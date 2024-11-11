6c9bbb31052d1665ac790786a9264aff423930ec
levitesuo <leevi.suotula@gmail.com>
2024-11-11 19:03:49 +0200

    modified something in indexa

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -8,15 +8,10 @@ logger("aloitetaan ohjelma")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-<<<<<<< HEAD
-print(f"{x} + {y} = {summa(x, y)}")  # muutos mainissa
-print(f"{x} - {y} = {erotus(x, y)}")  # muutos mainissa
-=======
 # muutos bugikorjaus-branchissa
 print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")
 # muutos bugikorjaus-branchissa
 print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")
->>>>>>> bugikorjaus
 
 logger("lopetetaan ohjelma")
 print("goodbye!")
